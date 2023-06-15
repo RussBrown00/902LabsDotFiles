0dec8795b1a34d5d5603f677e475ce0daaa6759c
Russell Brown <russ@902labs.com>
2023-06-15 11:10:55 -0400

    Fixing bug with nvchad reset script

--- nvchad/reset.sh
+++ nvchad/reset.sh
@@ -2,6 +2,6 @@ rm -rf ~/.config/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.cache/nvim
 
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 ln -s ~/.902LabsDotsFiles/nvchad/custom ~/.config/nvim/lua/custom
